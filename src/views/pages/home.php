5dec7774a5f4057c9df8e6feae8cde3fca2ea824
Braian Silva <braian.silva@advancetelecom.com.br>
2023-12-31 14:07:13 -0300

    Melhora no excluir e mudança de fonte

--- src/views/pages/home.php
+++ src/views/pages/home.php
@@ -16,12 +16,30 @@
             <td><?= $item['sorteio']; ?></td>
             <td><?= $item['tipo']; ?></td>
             <td>
-                <a href="<?=$base;?>/jogo/<?=$item['id'];?>/excluir" onclick="return confirm('Tem certeza que deseja excluir?')">
-                    <img width="20" src="<?=$base;?>/assets/images/excluir.png" alt="">
-                </a>
+            <a href="#" onclick="confirmDelete('<?=$item['id'];?>'); return false;">
+                <img width="20" src="<?=$base;?>/assets/images/excluir.png" alt="Excluir">
+            </a>
             </td>
         </tr>
     <?php endforeach; ?>
 </table>
+
+<script>
+    function confirmDelete(id) {
+        Swal.fire({
+            title: 'Tem certeza?',
+            text: 'Tem certeza que deseja excluir?',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Sim, excluir!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                window.location.href = '<?=$base;?>/jogo/' + id + '/excluir';
+            }
+        });
+    }
+</script>
 
 <?php $render('footer'); ?>
--- src/views/partials/headerInicial.php
+++ src/views/partials/headerInicial.php
@@ -3,6 +3,7 @@
         <title>Lotérica</title>
         <link rel ="icon" href="<?=$base;?>/assets/images/favicon.ico">
         <link rel="stylesheet" href="<?=$base;?>/assets/css/style.css">
+        <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     </head>
     <body>
         <header>
